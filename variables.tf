98b5d6e9b303a7e67ea805463fdd811e0e7e9655
softengrahmed <softengrahmed@gmail.com>
2025-03-06 23:14:44 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "environment" {
+  description = "The environment for the resources"
+  type        = string
+  default     = "dev"
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "my-cluster"
+}
+
+variable "rds_identifier" {
+  description = "The identifier for the RDS instance"
+  type        = string
+  default     = "my-rds"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "mydatabase"
+}
+
+variable "db_username" {
+  description = "The username for the database"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  default     = "password"
+  sensitive   = true
+}
